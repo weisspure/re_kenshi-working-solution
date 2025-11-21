e3c4de2b4539f8c5a84284e118d79eb642931395
Bryden Frizzell <bryden.frizzell@gmail.com>
2025-11-22 10:48:14 +1300

    Cleanup + added more conditions

--- WorldStates/WorldStates.cpp
+++ WorldStates/WorldStates.cpp
@@ -1,5 +1,4 @@
 
-#include <ogre/OgreMemorySTLAllocator.h>
 #include <kenshi/WorldEventStateQuery.h>
 #include <kenshi/GameData.h>
 #include <kenshi/GameWorld.h>
@@ -16,6 +15,8 @@ enum itemTypeExtended
 	VARIABLE = 1000
 };
 
+// WorldEventStateQuery objects don't store a ref to their gamedata so we need this to get it in WorldEventStateQuery::isTrue
+// I'm currently not doing garbage collection on this but that should just cause a small memory leak when reloading, not enough to care about
 boost::unordered_map<WorldEventStateQuery*, GameData*> queryMap;
 // probably unnecessary  but I don't want to take the risk
 boost::mutex mapLock;
@@ -39,49 +40,48 @@ bool checkTags_hook(DialogLineData* thisptr, Character* me, Character* target)
 {
 	bool returnVal = checkTags_orig(thisptr, me, target);
 
-	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator varEquals = thisptr->getGameData()->objectReferences.find("variable equals");
-	if (varEquals != thisptr->getGameData()->objectReferences.end())
+	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator iter = thisptr->getGameData()->objectReferences.find("variable equals");
+	if (iter != thisptr->getGameData()->objectReferences.end())
 	{
-		//if (thisptr->texts)
-		//	DebugLog(thisptr->texts[0]);
-		for (Ogre::vector<GameDataReference>::type::iterator iter = varEquals->second.begin(); iter != varEquals->second.end(); ++iter)
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
 		{
-			// weird hack - save data doesn't get loaded onto the GameData object referenced here, so we need to load the actual saved value from the save gamedatas
-			/*
-			ogre_unordered_map<std::string, GameData*>::type::iterator saveDataIter = ou->gamedata.gamedataSID.find(iter->sid);
-			if (saveDataIter != ou->gamedata.gamedataSID.end())
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
+			if (valueIter != variableIter->ptr->idata.end())
 			{
-				ogre_unordered_map<std::string, int>::type::iterator valueIter = saveDataIter->second->idata.find("value");
-				int value = -1;
-				if (valueIter != saveDataIter->second->idata.end())
-					value = valueIter->second;
-			/*
-			DebugLog("Ref: " +  iter->ptr->name);
-			DebugLog("Val1: " + std::to_string((int64_t)iter->values[0]));
-			DebugLog("Val2: " + std::to_string((int64_t)iter->values[1]));
-			DebugLog("Val3: " + std::to_string((int64_t)iter->values[2]));
-			DebugLog("SID: " + iter->sid);
-			*/
-			ogre_unordered_map<std::string, int>::type::iterator valueIter = iter->ptr->idata.find("value");
-			if (valueIter != iter->ptr->idata.end())
-			{
-				//DebugLog("Value: " + std::to_string((int64_t)valueIter->second));
 				// apply condition
-				returnVal = returnVal && iter->values[0] == valueIter->second;
+				returnVal = returnVal && valueIter->second == variableIter->values[0];
 			}
 		}
 	}
-	/*
-	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator iter = thisptr->getGameData()->objectReferences.begin();
-	for (; iter != thisptr->getGameData()->objectReferences.end(); ++iter)
+
+	iter = thisptr->getGameData()->objectReferences.find("variable less than");
+	if (iter != thisptr->getGameData()->objectReferences.end())
 	{
-		DebugLog(iter->first);
-		for (Ogre::vector<GameDataReference>::type::iterator iter2 = iter->second.begin(); iter2 != iter->second.end(); ++iter2)
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
 		{
-			DebugLog(iter2->sid);
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
+			if (valueIter != variableIter->ptr->idata.end())
+			{
+				// apply condition
+				returnVal = returnVal && valueIter->second < variableIter->values[0];
+			}
 		}
 	}
-	*/
+
+	iter = thisptr->getGameData()->objectReferences.find("variable greater than");
+	if (iter != thisptr->getGameData()->objectReferences.end())
+	{
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
+		{
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
+			if (valueIter != variableIter->ptr->idata.end())
+			{
+				// apply condition
+				returnVal = returnVal && valueIter->second > variableIter->values[0] ;
+			}
+		}
+	}
+
 	return returnVal;
 }
 
@@ -99,19 +99,16 @@ bool isTrue_hook(WorldEventStateQuery* thisptr)
 	}
 
 	// our new conditions
-	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator iter = gameData->objectReferences.find("variable is");
+	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator iter = gameData->objectReferences.find("variable equals");
 	if(iter != gameData->objectReferences.end())
 	{
-		//DebugLog("Ref name: " + iter->first);
-		for (Ogre::vector<GameDataReference>::type::iterator iter2 = iter->second.begin(); iter2 != iter->second.end(); ++iter2)
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
 		{
-			//DebugLog("Ref val name: " + iter2->sid);
-			int targetVal = iter2->values[0];
-			ogre_unordered_map<std::string, int>::type::iterator valueIter = iter2->ptr->idata.find("value");
+			int targetVal = variableIter->values[0];
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
 
-			if (valueIter != iter2->ptr->idata.end())
+			if (valueIter != variableIter->ptr->idata.end())
 			{
-				//DebugLog("Check " + std::to_string((int64_t)targetVal) + " " + std::to_string((uint64_t)valueIter->second));
 				state = state && (targetVal == valueIter->second);
 			}
 			else
@@ -120,88 +117,89 @@ bool isTrue_hook(WorldEventStateQuery* thisptr)
 			}
 		}
 	}
+
+	iter = gameData->objectReferences.find("variable less than");
+	if (iter != gameData->objectReferences.end())
+	{
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
+		{
+			int targetVal = variableIter->values[0];
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
+
+			if (valueIter != variableIter->ptr->idata.end())
+			{
+				state = state && (targetVal < valueIter->second);
+			}
+			else
+			{
+				ErrorLog("WorldStates: Variable is missing value");
+			}
+		}
+	}
+
+	iter = gameData->objectReferences.find("variable greater than");
+	if (iter != gameData->objectReferences.end())
+	{
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
+		{
+			int targetVal = variableIter->values[0];
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
+
+			if (valueIter != variableIter->ptr->idata.end())
+			{
+				state = state && (targetVal > valueIter->second);
+			}
+			else
+			{
+				ErrorLog("WorldStates: Variable is missing value");
+			}
+		}
+	}
+
 	return state;
 }
 
 void (*_doActions_orig)(Dialogue* thisptr, DialogLineData* dialogLine);
 void _doActions_hook(Dialogue* thisptr, DialogLineData* dialogLine)
 {
-	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator setVar = dialogLine->getGameData()->objectReferences.find("set variable");
-	if (setVar != dialogLine->getGameData()->objectReferences.end())
+	ogre_unordered_map<std::string, Ogre::vector<GameDataReference>::type>::type::iterator iter = dialogLine->getGameData()->objectReferences.find("set variable");
+	if (iter != dialogLine->getGameData()->objectReferences.end())
 	{
-		if (setVar->second.size() == 0)
+		if (iter->second.size() == 0)
 		{
 			ErrorLog("Missing references for \"set variable\"");
 		}
 
-		for (Ogre::vector<GameDataReference>::type::iterator iter = setVar->second.begin(); iter != setVar->second.end(); ++iter)
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
 		{
-			/*
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
+
+			if (valueIter != variableIter->ptr->idata.end())
 			{
-				DebugLog("Finding before: " + iter->sid);
-				ogre_unordered_map<std::string, GameData*>::type::iterator saveDataIter = ou->savedata.gamedataSID.find(iter->sid);
-				if (saveDataIter != ou->savedata.gamedataSID.end())
-				{
-					ogre_unordered_map<std::string, int>::type::iterator valueIter = saveDataIter->second->idata.find("value");
-					int value = -1;
-					if (valueIter != saveDataIter->second->idata.end())
-						value = valueIter->second;
-					DebugLog(saveDataIter->second->stringID + " " + saveDataIter->second->name + " " + std::to_string((int64_t)saveDataIter->second->type) + " " + std::to_string((int64_t)value));
-				}
-				else
-					DebugLog("Not found!");
-				saveDataIter = ou->gamedata.gamedataSID.find(iter->sid);
-				if (saveDataIter != ou->gamedata.gamedataSID.end())
-				{
-					ogre_unordered_map<std::string, int>::type::iterator valueIter = saveDataIter->second->idata.find("value");
-					int value = -1;
-					if (valueIter != saveDataIter->second->idata.end())
-						value = valueIter->second;
-					DebugLog(saveDataIter->second->stringID + " " + saveDataIter->second->name + " " + std::to_string((int64_t)saveDataIter->second->type) + " " + std::to_string((int64_t)value));
-				}
-				else
-					DebugLog("Not found!");
+				valueIter->second = variableIter->values[0];
 			}
-			*/
-			ogre_unordered_map<std::string, int>::type::iterator valueIter = iter->ptr->idata.find("value");
-
-			if (valueIter != iter->ptr->idata.end())
+			else
 			{
-				// TODO persistant state
+				ErrorLog("Missing parameter: value");
+			}
+		}
+	}
 
+	iter = dialogLine->getGameData()->objectReferences.find("add to variable");
+	if (iter != dialogLine->getGameData()->objectReferences.end())
+	{
+		if (iter->second.size() == 0)
+		{
+			ErrorLog("Missing references for \"add to variable\"");
+		}
 
-				DebugLog("Referenced value: " + std::to_string((int64_t)valueIter->second));
-				valueIter->second = iter->values[0];
+		for (Ogre::vector<GameDataReference>::type::iterator variableIter = iter->second.begin(); variableIter != iter->second.end(); ++variableIter)
+		{
+			ogre_unordered_map<std::string, int>::type::iterator valueIter = variableIter->ptr->idata.find("value");
 
-				/*
-				//ou->gamedata.updateData(iter->ptr, true);
-				//ou->savedata.addNewData(iter->ptr, iter->ptr->stringID);
-				{
-					DebugLog("Finding after: " + iter->sid);
-					ogre_unordered_map<std::string, GameData*>::type::iterator saveDataIter = ou->savedata.gamedataSID.find(iter->sid);
-					if (saveDataIter != ou->savedata.gamedataSID.end())
-					{
-						ogre_unordered_map<std::string, int>::type::iterator valueIter = saveDataIter->second->idata.find("value");
-						int value = -1;
-						if (valueIter != saveDataIter->second->idata.end())
-							value = valueIter->second;
-						DebugLog(saveDataIter->second->stringID + " " + saveDataIter->second->name + " " + std::to_string((int64_t)saveDataIter->second->type) + " " + std::to_string((int64_t)value));
-					}
-					else
-						DebugLog("Not found!");
-					saveDataIter = ou->gamedata.gamedataSID.find(iter->sid);
-					if (saveDataIter != ou->gamedata.gamedataSID.end())
-					{
-						ogre_unordered_map<std::string, int>::type::iterator valueIter = saveDataIter->second->idata.find("value");
-						int value = -1;
-						if (valueIter != saveDataIter->second->idata.end())
-							value = valueIter->second;
-						DebugLog(saveDataIter->second->stringID + " " + saveDataIter->second->name + " " + std::to_string((int64_t)saveDataIter->second->type) + " " + std::to_string((int64_t)value));
-					}
-					else
-						DebugLog("Not found!");
-				}
-				*/
+			if (valueIter != variableIter->ptr->idata.end())
+			{
+				valueIter->second += variableIter->values[0];
 			}
 			else
 			{
@@ -213,102 +211,42 @@ void _doActions_hook(Dialogue* thisptr, DialogLineData* dialogLine)
 }
 
 // this is a convenient place to hook into the save system
+// not 100% sure this is the best way to save data - data here is written to "quick.save"
 void (*saveGameState_orig)(FactionManager* thisptr, GameDataContainer* container);
 void saveGameState_hook(FactionManager* thisptr, GameDataContainer* container)
 {
-	//DebugLog("SAVING");
-	// get variables
-	// this doesn't seem to work
-	/*
-	lektor<GameData*> gameDataList;
-	ou->savedata.getDataOfType(gameDataList, (itemType)VARIABLE);
-	for (int i = 0; i < gameDataList.size(); ++i)
-		DebugLog(gameDataList[i]->name);
-		*/
-	//container->readOnly = false;
-	//container->isBaseDatafile = !container->isBaseDatafile;
-
-	//DebugLog(std::to_string((uint64_t)container->gamedataSID.size()));
+	// find VARIABLE gamedata
 	ogre_unordered_map<std::string, GameData*>::type::iterator iter = ou->gamedata.gamedataSID.begin();
 	for (; iter != ou->gamedata.gamedataSID.end(); ++iter)
 	{
-		//DebugLog(iter->first + " " + std::to_string((uint64_t)iter->second->type));
 		if (iter->second->type == (itemType)VARIABLE)
 		{
-			//container->addNewData(iter->second, iter->second->stringID);
-			/*
-			lektor<GameData*> tempData;
-			GameData* tempPtr = (*iter);
-			tempData.count = 1;
-			tempData.maxSize = 1;
-			tempData.stuff = &tempPtr;
-			container->addNewData(tempData);
-			*/
-			// TODO update other maps?
-			//container->mainList.insert((*iter));
-			/*
-			GameData* newGameData = container->createNewData((itemType)VARIABLE, iter->second->stringID, iter->second->name);
-			if (newGameData)
-			{
-				DebugLog("Added");
-				DebugLog(newGameData->name);
-				DebugLog(newGameData->stringID);
-			}
-			else
-			{
-				DebugLog("Could not create");
-			}
-			*/
+			// create new GameData in save
 			GameData* newGameData2 = container->createNewData(iter->second->type, iter->second->stringID, iter->second->name);
 			if (newGameData2)
 				newGameData2->updateFrom(iter->second, false);
-			//container->mainList.emplace((*iter));
-			// this is technically illegal
-			//GameDataManager* casted = (GameDataManager*)container;
-			//casted->updateData((*iter), true);
 		}
 	}
-	//DebugLog(std::to_string((uint64_t)container->gamedataSID.size()));
-
-	//DebugLog(container->name);
-	/*
-	iter = container->mainList.begin();
-	for (; iter != container->mainList.end(); ++iter)
-		if ((*iter)->type == (itemType)VARIABLE)
-			DebugLog("Contains 1: " + (*iter)->name);
-			*/
-	// TODO leveleditor?
-	//container->addNewData()
 	saveGameState_orig(thisptr, container);
-	/*
-	iter = container->mainList.begin();
-	for (; iter != container->mainList.end(); ++iter)
-	{
-		DebugLog((*iter)->name);
-		if ((*iter)->type == (itemType)VARIABLE)
-			DebugLog("Contains 2: " + (*iter)->name);
-	}*/
 }
 
 
+// save data is loaded into ou->savedata but the dialogue/world state reference is to the object in ou->gamedata
+// so we copy the value over on load
+// this probably isn't the ideal function to hook but as far as I can tell it's only called when loading a save game
 void (*loadAllPlatoons_orig)(GameWorld* thisptr);
 void loadAllPlatoons_hook(GameWorld* thisptr)
 {
 	loadAllPlatoons_orig(thisptr);
-	DebugLog("Platoons Load");
+
 	ogre_unordered_map<std::string, GameData*>::type::iterator iter = ou->savedata.gamedataSID.begin();
 	for (; iter != ou->savedata.gamedataSID.end(); ++iter)
 	{
-		//DebugLog(iter->first + " " + std::to_string((uint64_t)iter->second->type));
 		if (iter->second->type == (itemType)VARIABLE)
 		{
-			DebugLog("Found variable " + iter->second->name);
-			DebugLog("Loading : " + std::to_string((int64_t)iter->second->idata.at("value")));
 			ou->gamedata.updateData(iter->second, false);
-			DebugLog("After Load: " + std::to_string((int64_t)ou->gamedata.gamedataSID.at(iter->first)->idata.at("value")));
 		}
 	}
-
 }
 
 __declspec(dllexport) void startPlugin()
