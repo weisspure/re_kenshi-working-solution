b934f4ad362998543c5cebadf8bed12750b4e368
Bryden Frizzell <bryden.frizzell@gmail.com>
2025-11-30 08:43:24 +1300

    Cleanup

--- Dialogue_FCS/DialoguePlugin.cs
+++ Dialogue_FCS/DialoguePlugin.cs
@@ -39,7 +39,6 @@ namespace Dialogue_FCS
 
 				object conditionDefaults = Traverse.Create(AccessTools.TypeByName("forgotten_construction_set.dialog.ConditionControl")).Field("conditionDefaults").GetValue();
 				
-				// AccessTools.TypeByName("forgotten_construction_set.DialogConditionEnum")
 				Type condDefType = typeof(Dictionary<,>).MakeGenericType(new Type[] { AccessTools.TypeByName("forgotten_construction_set.DialogConditionEnum"), typeof(object) });
 				MethodInfo method = condDefType.Method("Add", new Type[] { AccessTools.TypeByName("forgotten_construction_set.DialogConditionEnum"), typeof(object) });
 				condDefType.Method("Add", new Type[] { AccessTools.TypeByName("forgotten_construction_set.DialogConditionEnum"), typeof(object) })
@@ -48,75 +47,5 @@ namespace Dialogue_FCS
 					(AccessTools.TypeByName("forgotten_construction_set.CharacterPerceptionTags_ShortTerm").GetMember("ST_NONE").First() as FieldInfo).GetValue(null) });
 			}
 		}
-		/*
-		// patch to enable the condition tag box
-		[HarmonyPatch("forgotten_construction_set.dialog.ConditionControl", "refreshGrid")]
-		public static class ConditionControl_refreshGrid_Patch
-		{
-			[HarmonyPostfix]
-			static void Postfix(object __instance, object dialogLine)
-			{
-				if (dialogLine != null)
-				{
-					//ListViewItem listViewItem = this.listView1conditions.Items;
-					ListView listView1conditions = Traverse.Create(__instance).Field("listView1conditions").GetValue() as ListView;
-					foreach(ListViewItem item in listView1conditions.Items)
-					{
-						if(item.SubItems.Count >= 5)
-						{
-							// fix enum
-							IDictionary conditionDefaults = (IDictionary)Traverse.Create(AccessTools.TypeByName("forgotten_construction_set.dialog.ConditionControl")).Field("conditionDefaults").GetValue();
-							object conditionEnum = Enum.Parse(AccessTools.TypeByName("forgotten_construction_set.DialogConditionEnum"), item.SubItems[1].Text);
-							if(conditionDefaults.Contains(conditionEnum))
-							{
-								int result;
-								// if the value is an int instead of an enum name, convert it to it's enum
-								if(int.TryParse(item.SubItems[4].Text, out result))
-								{
-									item.SubItems.RemoveAt(4);
-
-									// use type of default value for enum parsing
-									object defaultVal = conditionDefaults[conditionEnum];
-									item.SubItems.Add(Enum.ToObject(defaultVal.GetType(), result).ToString());
-								}
-							}
-							else
-							{
-								item.SubItems.RemoveAt(4);
-							}
-
-						}
-					}
-				}
-			}
-			// this is fucked
-
-			enum NUMBER
-			{
-
-			}
-			// fixes parsing of dialogue condition tags
-			[HarmonyTranspiler]
-			static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-			{
-				Type personaltyTags = AccessTools.TypeByName("forgotten_construction_set.PersonalityTags");
-
-				foreach (var instruction in instructions)
-				{
-					if (instruction.opcode == OpCodes.Constrained)
-					{
-						Console.WriteLine(instruction);
-
-						if ((Type)instruction.operand == personaltyTags)
-						{
-							//instruction.opcode = OpCodes.Nop;
-							instruction.operand = typeof(NUMBER);// typeof(object);
-						}
-					}
-				}
-				return instructions;
-			}
-		}
-		*/
 	}
 }
